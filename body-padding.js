6a86429bb14c09e08b9e1907c289384e41a73574
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-07-31 15:53:08 +0300

    add comment

--- body-padding.js
+++ body-padding.js
@@ -1,5 +1,5 @@
 const { height: pageHeaderHeight } = document
-	.querySelector(".header")
+	.querySelector(".header") // подставляете класс вашего хедера
 	.getBoundingClientRect();
 
 document.body.style.paddingTop = `${pageHeaderHeight}px`;
